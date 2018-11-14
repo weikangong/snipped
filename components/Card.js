3ca5414299179a59caa156d234998da4650009d3
whatthestone <kaising.n@gmail.com>
2018-11-14 00:34:50 -0800

    updated card design

--- components/Card.js
+++ components/Card.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity
 } from 'react-native';
 import Dialog, { SlideAnimation, DialogContent, DialogTitle, DialogButton } from 'react-native-popup-dialog';
+import { Ionicons } from '@expo/vector-icons';
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 const WINDOW_HEIGHT = Dimensions.get('window').height;
@@ -40,6 +41,37 @@ export default class Card extends React.Component {
             <View>
               <Text style={styles.subtitle}> this is subtitle </Text>
             </View>
+
+            <View style={styles.infoRow}>
+              <View style={{width: 30, height: 30}}>
+                <Ionicons name="md-person" size={24} color="grey" />
+              </View>
+              <View>
+                <Text style={styles.subtext}>Janet Wong, Christina Choo and 98 others are also going</Text>
+              </View>
+            </View>
+
+            <View style={styles.infoRow}>
+              <View style={{width: 30, height: 30}}>
+                <Ionicons name="md-calendar" size={24} color="grey" />
+              </View>
+              <View>
+                <Text style={styles.subtext}>Sat, 19 Sep 2018</Text>
+                <Text style={styles.greysubtext}>10:00 AM - 5:00 AM GMT+8</Text>
+                <Text style={styles.link}>Add to Calendar</Text>
+              </View>
+            </View>
+
+            <View style={styles.infoRow}>
+              <View style={{width: 30, height: 30}}>
+                <Ionicons name="md-pin" size={24} color="grey" />
+              </View>
+              <View>
+                <Text style={styles.subtext}>Engineering Auditorium 6</Text>
+                <Text style={styles.greysubtext}>2706 Martin Luther King Jr. Way, Berkeley CA 94703</Text>
+              </View>
+            </View>
+
           </View>
         </View>
         <Dialog
@@ -64,14 +96,42 @@ export default class Card extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  infoBox: {
+  },
+  infoRow: {
+    padding: 15,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
   header: {
     fontFamily: 'Avenir',
     fontSize: 30,
     lineHeight: 50,
   },
+  subtext: {
+    fontFamily: 'Avenir',
+    fontSize: 16,
+    lineHeight: 25,
+    paddingLeft: 5,
+  },
+  greysubtext: {
+    fontFamily: 'Avenir',
+    color: 'grey',
+    fontSize: 16,
+    lineHeight: 25,
+    paddingLeft: 5,
+  },
+  link: {
+    fontFamily: 'Avenir',
+    color: '#0099CC',
+    fontSize: 16,
+    lineHeight: 25,
+    paddingLeft: 5,
+  },
   subtitle: {
     fontFamily: 'Avenir',
-    fontSize: 15,
+    fontSize: 14,
     lineHeight: 25,
     paddingLeft: 5,
   },
@@ -82,6 +142,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderWidth: 0,
     elevation: 1,
+    width: WINDOW_WIDTH-50,
   },
   top: {
     width: WINDOW_WIDTH-50,
@@ -94,8 +155,8 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   container: {
-     flex: 1,
-     alignItems: 'center',
-     justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
    },
 });
